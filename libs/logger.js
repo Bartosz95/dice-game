09fd29084947abebf71032b4d294d4dcfcb1624c
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2022-03-05 17:13:38 +0100

    Display logs to console

--- libs/logger.js
+++ libs/logger.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
   logger.add(new transports.File({ filename: 'logs/out.log'}))
   logger.add(new transports.File({ filename: 'logs/error.log', level: 'error'}))
 } else {
+  logger.add(new transports.Console())
   logger.add(new transports.File({ filename: '/var/log/dice-game-api/out.log'}))
   logger.add(new transports.File({ filename: '/var/log/dice-game-api/error.log', level: 'error'}))
 }
