58fffedb25ae54afa7040718ebae1c19fdea6593
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2022-03-22 22:23:53 +0100

    Delete Mug.js and Dice.js and change way to send requests

--- libs/game_modules/Game.js
+++ libs/game_modules/Game.js
@@ -1,30 +1,34 @@
-import Mug from './Mug';
-import Dice from './Dice';
 
-export default class Game {
-    constructor(numberOfDices, numberOfDiceSides, userIDs) {
+export const rollTheDices = (mug, dicesIndex) => {
+    for (let i = 0; i < dicesIndex.length; i++) {
+        mug[dicesIndex[i]] = Math.floor(Math.random() * 6) + 1 
+    }
+    return mug
+}
+
+export class Game {
+    constructor(userIDs) {
         this.isActive = true;
         this.numberOfTurn = 0
         this.currentUser = userIDs[Math.floor(Math.random() * userIDs.length)]
         this.currentRoll = 0
 
-        const dices = []
-        for (let i = 0; i < numberOfDices; i++) {
-            dices[i] = new Dice(numberOfDiceSides);
-        }
-        this.mug = new Mug(dices);
-        this.userIDs = userIDs;
-
-        this.table = {}
-        this.userIDs.forEach(id => {
-            this.table[id] = {   "1": null, "2": null, "3": null, "4": null, "5": null, "6": null, 
-                                "bonus": null, "3x": null, "4x": null, "full": null, 
-                                "small strit": null, "big strit": null, "general": null, "chance": null, 
-                                "sum": null, "total": null
+        this.users = {}
+        userIDs.forEach(id => {
+            this.users[id] = {
+                table : { "1": null, "2": null, "3": null, "4": null, "5": null, "6": null, 
+                        "bonus": null, "3x": null, "4x": null, "full": null, 
+                        "small strit": null, "big strit": null, "general": null, "chance": null, 
+                        "sum": null, "total": null
+                },
+                mug : rollTheDices( 
+                    { "0": null, "1": null, "2": null, "3": null, "4": null },
+                    [0,1,2,3,4]
+                )
             }
         });
-        
-         
     }
 }
+
+
 
--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 
 import logger from '../libs/logger';
-import Game from '../libs/game_modules/Game'
+import { Game, rollTheDices} from '../libs/game_modules/Game'
 import gameModel from '../libs/db_schemas/GameSchema'
-import Mug from '../libs/game_modules/Mug';
+//import Mug from '../libs/game_modules/Mug';
 
 const errorMessage = "Some problem occurence"
 
@@ -15,23 +15,8 @@ gameModel.find((err, docs) => {
       console.log(err);
     }
 
-    console.log(docs);
+    console.log(docs.length);
 })
-
-// gameModel.find({}, (err, docs) => {
-//     if(err) {
-//       logger.error(err)
-//     } else if(docs.length < 1) {
-//         const instance = new gameModel({game: new Game(5,6)})
-//         instance.save((err) => {
-//             if(err) {
-//                 logger.error(err)
-//             } else {
-//                 logger.info(`Created game ${instance._id}`)
-//             }
-//         });
-//     }
-//   });
 
 /*
 First add token to token enviroment variable
@@ -43,11 +28,13 @@ echo $token
 
 const router = Router();
 
-
 // get all games
 // curl -H 'authorization: Bearer $token' -H "Content-Type: application/json" -d '{"userID":"6224b5c30eac08007061fa31"}' http://localhost:3000/api/v1/game
-router.get('/game', (req, res) => {
-    gameModel.find({'game.userIDs': { $in: [req.body.userID]}} , (err, docs) => {
+router.get('/user/:userID/game', (req, res) => {
+    const userID = req.params.userID;
+
+    const userIDstring = `game.users.${userID}`;
+    gameModel.find({userIDstring : { $exists: true}} , (err, docs) => {
         if(err) {
           logger.error(err)
           res.send(errorMessage);
@@ -58,9 +45,12 @@ router.get('/game', (req, res) => {
 
 // get specific game
 // curl -H 'authorization: Bearer $token' -H "Content-Type: application/json" -d '{"userID":"6224b5c30eac08007061fa31"}' http://localhost:3000/api/v1/game/622cd907f6026dbf7cad27ef 
-router.get('/game/:id', (req, res) => {
-    const id = req.params.id;
-    gameModel.findOne({ _id: id, 'game.userIDs': { $in: [req.body.userID]}}, (err, docs) => {
+router.get('/user/:userID/game/:gameID', (req, res) => {
+    const userID = req.params.userID;
+    const gameID = req.params.gameID;
+
+    const userIDstring = `game.users.${userID}`;
+    gameModel.findOne({ _id: gameID, userIDstring : { $exists: true}}, (err, docs) => {
         if(err) {
           logger.error(err)
           res.send(errorMessage);
@@ -69,14 +59,14 @@ router.get('/game/:id', (req, res) => {
     });
 });
 
-
 // create a game
-// curl -d '{"numberOfDices":5, "numberOfDiceSides": 6, "userID":"6224b5c30eac08007061fa31", "userIDs":["1", "2"]}' -H "authorization: Bearer $token" -H "Content-Type: application/json" http://localhost:3000/api/v1/game 
-router.post('/game', (req, res) => {
-    const numberOfDices = req.body.numberOfDices;
-    const numberOfDiceSides = req.body.numberOfDiceSides;
-    const userIDs = req.body.userIDs.includes(req.body.userID) ? req.body.userIDs : req.body.userIDs.concat(req.body.userID);
-    const game = new Game(numberOfDices, numberOfDiceSides, userIDs)
+// curl --url http://localhost:3000/api/v1/user/1/game/623a2a1bab1114ac2afabd9c -d '{"numberOfDices":5, "numberOfDiceSides": 6, "userIDs":["1", "2"]}' -H "Content-Type: application/json" -H "authorization: Bearer $token" 
+router.post('/user/:userID/game', (req, res) => {
+    const userID = req.params.userID;
+    let userIDs = req.body.userIDs;
+
+    userIDs = userIDs.includes(userID) ? userIDs : userIDs.concat(userID); // add user to game if is not added in list
+    const game = new Game(userIDs)
     console.log(game)
     const instance = new gameModel({game});
     instance.save((err) => {
@@ -89,19 +79,17 @@ router.post('/game', (req, res) => {
         });
 });
 
+
 // roll the dices
 // curl -d '{"numbersToChange":[1,2]}' -H 'authorization: Bearer $token' -H "Content-Type: application/json"  http://localhost:3000/api/v1/game/622cd907f6026dbf7cad27ef 
-router.post('/game/:id', (req, res) => {
-    const id = req.params.id;
+router.post('/user/:userID/game/:gameID', (req, res) => {
+    const userID = req.params.userID;
+    const gameID = req.params.gameID;
     const numbersToChange = req.body.numbersToChange;
-    const userID = req.body.userID;
-    const response = {
-        status: null,
-        message: null,
-        object: null
-
-    }
-    gameModel.findOne({ _id: id, 'game.userIDs': { $in: [userID] }}, (err, doc) => {
+    const chosenFigure = req.body.chosenFigure;
+    
+    const userIDstring = `game.users.${userID}`;
+    gameModel.findOne({ _id: gameID, userIDstring : { $exists: true}}, (err, doc) => {
         if(err) {
           logger.error(err)
           res.send(errorMessage);
@@ -110,47 +98,63 @@ router.post('/game/:id', (req, res) => {
         const numberOfTurn = doc.game.numberOfTurn;
         const currentUser = doc.game.currentUser;
         const currentRoll = doc.game.currentRoll;
-        const userIDs = doc.game.userIDs;
-        const mug = new Mug(doc.game.mug.dices);
-        const replacement = {game: doc.game};
-
-        if(!isActive){
-            response.message="Game is over"
-        }
-         else if (currentUser !== userID) {
-            res.end("Is not user's turn")
-        }
-
-        if(currentRoll === 2) {
-            const userIndex = userIDs.indexOf(currentUser)
-            if(userIndex ===  userIDs.length -1 ) {
-                replacement.game.numberOfTurn = numberOfTurn + 1
-                if(numberOfTurn === 12) {
-                    logger.info(`Ended game: ${id}`)
-                    replacement.game.isActive = false
-                }
-            }
-            replacement.game.currentUser = userIDs[(userIndex + 1) % userIDs.length]
-            
-        }
-        replacement.game.currentRoll = (currentRoll + 1) % 3
+        const mug = doc.game.users[userID].mug;
+        const table = doc.game.users[userID].table
+        //const replacement = {game: doc.game};
         
-        mug.rollTheDices(numbersToChange)
-        replacement.game.mug = mug
+        if(!isActive){
+            res.send("Game is over")
+            return
+        }
+        if (currentUser !== userID) {
+            res.send("Is not user's turn")
+            return
+        }
 
-        gameModel.findByIdAndUpdate(id, replacement , (err, message) => {
-            if(err) {
-                logger.error(err);
-                res.send(errorMessage);
-            } else {
-                logger.info(`Updated game: ${id}`)
-                res.send(doc);
+        if(currentRoll === 0){
+            rollTheDices(mug, [0,1,2,3,4])
+        }
+        else if(chosenFigure){
+            if(table.hasOwnProperty(chosenFigure) && table[chosenFigure] === null) {
+                table[chosenFigure] = Object.values(mug)
             }
-        })
-        //
-    });
+        }
+        else if(numbersToChange) {
+            rollTheDices(mug, numbersToChange)
+        }
 
-    //res.send(response)
+        
+
+    //     if(currentRoll > 2) {
+    //         const userIndex = userIDs.indexOf(currentUser)
+    //         if(userIndex ===  userIDs.length -1 ) {
+    //             replacement.game.numberOfTurn = numberOfTurn + 1
+    //             if(numberOfTurn === 12) {
+    //                 logger.info(`Ended game: ${id}`)
+    //                 replacement.game.isActive = false
+    //             }
+    //         }
+    //         replacement.game.currentUser = userIDs[(userIndex + 1) % userIDs.length]
+            
+    //     }
+    //     replacement.game.currentRoll = (currentRoll + 1) % 4
+        
+ 
+
+    //     gameModel.findByIdAndUpdate(id, replacement , (err, message) => {
+    //         if(err) {
+    //             logger.error(err);
+    //             res.send(errorMessage);
+    //             return
+    //         } else {
+    //             logger.info(`Updated game: ${id}`)
+    //             res.send(doc);
+    //         }
+    //     })
+    console.log(mug)
+    console.log(table)
+    res.send(doc)
+    });
 });
 
 export default router;
