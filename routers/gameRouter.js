a74f021a53bf6ca0c2550d32457532fb9538ee02
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2022-03-26 03:54:15 +0100

    Extract db functions to file

--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -1,22 +1,7 @@
 import { Router } from 'express'
-
 import logger from '../libs/logger';
 import { Game, makeMove } from '../libs/Game'
-import gameModel from '../libs/GameSchema'
-//import Mug from '../libs/game_modules/Mug';
-
-// create a game if it not exist
-//gameModel.deleteMany({__v: 0}, function (err) {})
-gameModel.find((err, docs) => {
-    if(err) {
-      logger.error(err)
-      console.log(err);
-    }
-
-    console.log(docs.length);
-})
-
-const errorMessage = "Some problem occurence";
+import { getAllGames, getParticularGame, createGame, updateGame, deleteAllGames, deleteParticularGames } from '../libs/GameSchema'
 
 /*
 First add token to token enviroment variable
@@ -32,15 +17,12 @@ const router = Router();
 // curl -H 'authorization: Bearer $token' -H "Content-Type: application/json" -d '{"userID":"6224b5c30eac08007061fa31"}' http://localhost:3000/api/v1/game
 router.get('/user/:userID/game', (req, res) => {
     const userID = req.params.userID;
-
-    const userIDstring = `game.users.${userID}`;
-    gameModel.find({userIDstring : { $exists: true}} , (err, docs) => {
-        if(err) {
-          logger.error(err)
-          res.send(errorMessage);
+    getAllGames(userID, (errorMessage, games) => {
+        if(errorMessage) {
+            res.send(errorMessage);
         }
-        res.send(docs);
-      });
+        res.send(games);
+    })
 });
 
 // get specific game
@@ -48,35 +30,27 @@ router.get('/user/:userID/game', (req, res) => {
 router.get('/user/:userID/game/:gameID', (req, res) => {
     const userID = req.params.userID;
     const gameID = req.params.gameID;
-
-    const userIDstring = `game.users.${userID}`;
-    gameModel.findOne({ _id: gameID, userIDstring : { $exists: true}}, (err, docs) => {
-        if(err) {
-          logger.error(err)
-          res.send(errorMessage);
+    getParticularGame(userID, gameID, (errorMessage, game) => {
+        if(errorMessage) {
+            return res.send(errorMessage);
         }
-        res.send(docs);
-    });
+        res.send(game);
+    })
 });
 
 // create a game
 // curl --url http://localhost:3000/api/v1/user/1/game/623a2a1bab1114ac2afabd9c -d '{"numberOfDices":5, "numberOfDiceSides": 6, "userIDs":["1", "2"]}' -H "Content-Type: application/json" -H "authorization: Bearer $token" 
 router.post('/user/:userID/game', (req, res) => {
-    const userID = req.params.userID;
+    const userID = req.params.userID; 
     let userIDs = req.body.userIDs;
-
     userIDs = userIDs.includes(userID) ? userIDs : userIDs.concat(userID); // add user to game if is not added in list
     const game = new Game(userIDs)
-    console.log(game)
-    const instance = new gameModel({game});
-    instance.save((err) => {
-            if(err) {
-                logger.error(err)
-            } else {
-                logger.info(`Created game ${instance._id}`)
-                res.send(instance);
-            }
-        });
+    createGame(game, (errorMessage, game) => {
+        if(errorMessage) {
+            return res.send(errorMessage);
+        }
+        res.send(game);
+    })
 });
 
 
@@ -87,58 +61,42 @@ router.post('/user/:userID/game/:gameID', (req, res) => {
     const gameID = req.params.gameID;
     const numbersToChange = req.body.numbersToChange;
     const chosenFigure = req.body.chosenFigure;
-    
-    const userIDstring = `game.users.${userID}`;
-    
-    gameModel.findOne({ _id: gameID, userIDstring : { $exists: true}}, (error, doc) => {
-
-        if(error) {
-          logger.error(error)
-          res.send(errorMessage);
+    getParticularGame(userID, gameID, (errorMessage, doc) => {
+        if(errorMessage) {
+            logger.error(errorMessage)
+            res.send(errorMessage);
         }
-
         const game = doc.game;
-        
-
-        makeMove(game, userID, numbersToChange, chosenFigure, (gameErrorMessage, game) => {
-            if(gameErrorMessage || (game === 'null') ) {
-                logger.error(`Player ${userID} in game ${gameID} has error message: ${gameErrorMessage}`)
-                res.send(gameErrorMessage + '\n')
-            } else {
-                gameModel.findByIdAndUpdate(gameID, {game: game} , (error, message) => {
-                    if(error) {
-                        logger.error(error);
-                        res.send(errorMessage);
-                    } 
-                    logger.info(`Player ${userID} make a move in game: ${gameID}`)
-                    res.send(doc);
-                })
+        makeMove(game, userID, numbersToChange, chosenFigure, (errorMessage, game) => {
+            if(errorMessage || (game === 'null') ) {
+                logger.error(`Player ${userID} in game ${gameID} has error message: ${errorMessage}`)
+                res.send(errorMessage)
             }
-            console.log(doc.game)
-            console.log(doc.game.users["1"].table)
+            updateGame(gameID, game, (errorMessage) => {
+                if(errorMessage) {
+                    logger.error(errorMessage);
+                } else {
+                    logger.info(`Player ${userID} make a move in game: ${gameID}`)
+                }
+                res.send(errorMessage || doc);
+            })
         })
     })
 })
 
 router.delete('/user/:userID/game', (req, res) => {
     const userID = req.params.userID;
-
-    const userIDstring = `game.users.${userID}`;
-    gameModel.deleteMany({userIDstring : { $exists: true}}, (error) => {
-        logger.error(error); 
+    deleteAllGames(userID, (errorMessage) => {
+        res.send(errorMessage || `Delete all games for user ${userID}`)
     })
-    res.send(`Delete all games for user ${userID}`);
 });
 
 router.delete('/user/:userID/game/:gameID', (req, res) => {
     const userID = req.params.userID;
     const gameID = req.params.gameID;
-
-    const userIDstring = `game.users.${userID}`;
-    const numberOfGames = gameModel.deleteOne({ _id: gameID, userIDstring : { $exists: true}}, (error) => {
-        logger.error(error); 
+    deleteParticularGames(userID, gameID, (errorMessage) => {
+        res.send(errorMessage || `Delete ${gameID} games`);
     })
-    res.send(`Delete ${numberOfGames} games`);
 });
 
 
