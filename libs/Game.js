f112dafbb16e607fab18fc2cc4b9888a46dac681
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2022-03-28 23:06:27 +0200

    Rewrited game's alghoritm to Promises

--- libs/Game.js
+++ libs/Game.js
@@ -1,17 +1,17 @@
 export class Game {
-    constructor(userIDs) {
+    constructor(playerIDs) {
         this.isActive = true;
         this.numberOfRoll = 0;
         this.numberOfTurn = 0;
-        this.userIDs = userIDs;
-        this.currentUser = userIDs[Math.floor(Math.random() * this.userIDs.length)];
-        this.indexOfFirstPlayer = userIDs.indexOf(this.currentUser)
+        this.playerIDs = playerIDs;
+        this.currentPlayer = playerIDs[Math.floor(Math.random() * this.playerIDs.length)];
+        this.indexOfFirstPlayer = playerIDs.indexOf(this.currentPlayer)
 
         this.mug = { "0": null, "1": null, "2": null, "3": null, "4": null }
 
-        this.users = {}
-        userIDs.forEach(id => {
-            this.users[id] = {
+        this.players = {}
+        playerIDs.forEach(id => {
+            this.players[id] = {
                 table : { "1": null, "2": null, "3": null, "4": null, "5": null, "6": null, 
                         "bonus": null, "3x": null, "4x": null, "full": null, 
                         "small strit": null, "big strit": null, "general": null, "chance": null, 
@@ -23,137 +23,162 @@ export class Game {
     }
 }
 
-export const rollTheDices = (game, dicesIndex, callback) => {
-    
-    if(dicesIndex.filter(index => (index > 4) || (index < 0) || (Math.floor(index) != index) ).length > 0) {
-        return callback("Dices indexes are { 0, 1, 2, 3, 4 }", null)
-    }
-    const mug = game.mug;
+const rollTheDices = (mug, dicesIndex) => {
+    return new Promise( (resolve, reject) => {
 
-    for (let i = 0; i < dicesIndex.length; i++) {
-        mug[dicesIndex[i]] = Math.floor(Math.random() * 6) + 1
-    }
-    game.numberOfRoll += 1
-
-    callback(null, game)
-}
-
-export const countFigure = (game, chosenFigure) => {
-
-    const userID = game.currentUser;
-    const mug = Object.values(game.mug);
-    const table = game.users[userID].table;
-
-    const counter = [null,0,0,0,0,0,0]
-    mug.forEach(dice => {
-        counter[dice] +=1;
+        if(dicesIndex.filter(index => (index > 4) || (index < 0) || (Math.floor(index) != index) ).length > 0) {
+            return reject("Dices indexes are { 0, 1, 2, 3, 4 }")
+        }
+        for (let i = 0; i < dicesIndex.length; i++) {
+            mug[dicesIndex[i]] = Math.floor(Math.random() * 6) + 1
+        }
+        resolve(mug)
     })
-
-    switch(chosenFigure) {
-        case "1":
-            table[chosenFigure] = counter[chosenFigure] * chosenFigure;
-            break;
-        case "2":
-            table[chosenFigure] = counter[chosenFigure] * chosenFigure;
-            break;
-        case "3":
-            table[chosenFigure] = counter[chosenFigure] * chosenFigure;
-            break;
-        case "4":
-            table[chosenFigure] = counter[chosenFigure] * chosenFigure;
-            break;
-        case "5":
-            table[chosenFigure] = counter[chosenFigure] * chosenFigure;
-            break;
-        case "6":
-            table[chosenFigure] = counter[chosenFigure] * chosenFigure;
-            break;
-        case "3x":
-            table[chosenFigure] = counter.map(number => number >= 3).includes(true) ? mug.reduce((sum, dice) => sum + dice, 0) : 0;
-            break;
-        case "4x":
-            table[chosenFigure] = counter.map(number => number >= 4).includes(true) ? mug.reduce((sum, dice) => sum + dice, 0) : 0;
-            break;
-        case "full":
-            table[chosenFigure] = ( counter.includes(3) && counter.includes(2) ) ? 25 : 0;
-            break;
-        case "small strit":
-            table[chosenFigure] = ( counter[1] && counter[2] && counter[3] && counter[4] && counter[5] ) ? 30 : 0;
-            break;
-        case "big strit":
-            table[chosenFigure] = ( counter[2] && counter[3] && counter[4] && counter[5] && counter[6] ) ? 40 : 0;
-            break;
-        case "general":
-            table[chosenFigure] = counter.includes(5) ? 50 : 0;
-            break;
-        case "chance": 
-            table[chosenFigure] = mug.reduce((sum, dice) => sum + dice, 0);
-            break;
-    }
 }
 
-export const saveFigure = (game, chosenFigure, callback) => {
-
-    const userID = game.currentUser;
-    const userIDs = game.userIDs;
-    const numberOfTurn = game.numberOfTurn;
-    const table = game.users[userID].table;
-    const indexOfFirstPlayer = game.indexOfFirstPlayer;
-
-    if(!table.hasOwnProperty(chosenFigure) || 
-        chosenFigure === "bonus" ||
-        chosenFigure === "sum" ||
-        chosenFigure === "total" ) {
-        return callback(`You cannot choose figure: ${chosenFigure}`, null)
-    } else if (table[chosenFigure] !== null) {
-        return callback("Figure already chosen", null)
-    }
-
-    countFigure(game, chosenFigure)
-    game.numberOfRoll = 0
-    const nextUserIndex = (userIDs.indexOf(userID) + 1) % userIDs.length
-    game.currentUser = userIDs[nextUserIndex]
-    if(nextUserIndex === indexOfFirstPlayer) {
-        game.numberOfTurn += 1
-    }
-    
-    if(numberOfTurn > 13) {
-        game.isActive = false
-    }
-
-    callback(null, game)
-
+const saveFigure = (table, chosenFigure, result) => {
+    return new Promise((resolve, reject) => {
+        if(!table.hasOwnProperty(chosenFigure) || 
+            chosenFigure === "bonus" ||
+            chosenFigure === "sum" ||
+            chosenFigure === "total" ) {
+            reject(`You cannot choose figure: ${chosenFigure}`)
+        } else if (table[chosenFigure] !== null) {
+            reject("Figure already chosen")
+        }
+        table[chosenFigure] = result
+        resolve(table)
+        
+    })
 }
 
-export const makeMove = (game, userID, numbersToChange, chosenFigure, callback) => {
+const countResult = (mug, chosenFigure) => {
+    return new Promise( (resolve, reject ) => {
+        
+        const countMug = Object.values(mug);
+        const counter = [null,0,0,0,0,0,0]
+        countMug.forEach(dice => {
+            counter[dice] +=1;
+        })
+        switch(chosenFigure) {
+            case "1":
+                resolve(counter[chosenFigure] * chosenFigure);
+                break;
+            case "2":
+                resolve(counter[chosenFigure] * chosenFigure);
+                break;
+            case "3":
+                resolve(counter[chosenFigure] * chosenFigure);
+                break;
+            case "4":
+                resolve(counter[chosenFigure] * chosenFigure);
+                break;
+            case "5":
+                resolve(counter[chosenFigure] * chosenFigure);
+                break;
+            case "6":
+                resolve(counter[chosenFigure] * chosenFigure);
+                break;
+            case "3x":
+                resolve(counter.map(number => number >= 3).includes(true) ? countMug.reduce((sum, dice) => sum + dice, 0) : 0);
+                break;
+            case "4x":
+                resolve(counter.map(number => number >= 4).includes(true) ? countMug.reduce((sum, dice) => sum + dice, 0) : 0);
+                break;
+            case "full":
+                resolve((counter.includes(3) && counter.includes(2) ) ? 25 : 0);
+                break;
+            case "small strit":
+                resolve((counter[1] && counter[2] && counter[3] && counter[4] && counter[5] ) ? 30 : 0);
+                break;
+            case "big strit":
+                resolve((counter[2] && counter[3] && counter[4] && counter[5] && counter[6] ) ? 40 : 0);
+                break;
+            case "general":
+                resolve(counter.includes(5) ? 50 : 0);
+                break;
+            case "chance": 
+                resolve(countMug.reduce((sum, dice) => sum + dice, 0));
+                break;
+            default:
+                reject("Figure does not exist")
+        }
+        
+    })
+}
+
+const getNextPlayer = (playerIDs, currentPlayer) =>{
+    const nextUserIndex = (playerIDs.indexOf(currentPlayer) + 1) % playerIDs.length
+    return playerIDs[nextUserIndex]
+}
+
+const getTurn = (playerIDs, currentPlayer, indexOfFirstPlayer) => {
+    return playerIDs.indexOf(currentPlayer) === indexOfFirstPlayer ? 1 : 0
+}
+
+const isGameEnd = (numberOfTurn) => {
+    return numberOfTurn > 13
+}
+
+const move = (playerID, isActive, currentPlayer, numberOfRoll, dicesToChange, chosenFigure) => {
+    return new Promise((resolve, reject) => {
+
+        if(!isActive){
+            reject('Game is over')
+        }
+        if (playerID !== currentPlayer) {
+            reject(`This is turn of user: ${currentPlayer}`)
+        }
+        if(numberOfRoll === 0) {
+            resolve("rollAllDices")
+        } 
+        else if(chosenFigure) {
+            console.log("here")
+            resolve("saveFigure")
+        }
+        else if (numberOfRoll < 3) {
+            if (!dicesToChange){
+                reject("You have to choose dice to rool on choose a figure")
+            }
+            resolve("rollDices")
+        } 
+        else {
+            if(!chosenFigure) {
+                reject("You have to choose a figure")
+            }
+            resolve("saveFigure")
+        }
+    })
+}
+
+export async function makeMove(game, userID, dicesToChange, chosenFigure) {
 
     const isActive = game.isActive;
-    const currentUser = game.currentUser;
     const numberOfRoll = game.numberOfRoll;
+    const numberOfTurn = game.numberOfTurn;
+    const currentPlayer = game.currentPlayer;
+    const mug = game.mug
+    const playerIDs = game.playerIDs
+    const indexOfFirstPlayer = game.indexOfFirstPlayer
+    const currentPlayerTable = game.players[userID].table
 
-    if(!isActive){
-        return callback('Game is over', null)
-    }
-    if (userID !== currentUser) {
-        return callback(`This is turn of user: ${currentUser}`, null)
-    }
-
-    if(numberOfRoll === 0) {
-        rollTheDices(game, [0,1,2,3,4], callback)
-    } 
-    else if(chosenFigure) {
-        saveFigure(game, chosenFigure, callback)
-    }
-    else if (numberOfRoll < 3) {
-        if (!numbersToChange){
-            return callback("You have to choose dice to rool on choose a figure", null)
-        }
-        rollTheDices(game, numbersToChange, callback)
-    } 
-    else {
-        if(!chosenFigure) {
-            return callback("You have to choose a figure", null)
-        }
-        saveFigure(game, chosenFigure, callback)
+    const whatToDo = await move(userID, isActive, currentPlayer, numberOfRoll, dicesToChange, chosenFigure)
+    switch(whatToDo) {
+        case 'rollDices':
+            game.mug = await rollTheDices(mug, dicesToChange)
+            game.numberOfRoll += 1
+            break;
+        case 'rollAllDices':
+            game.mug = await rollTheDices(mug, [0,1,2,3,4])
+            game.numberOfRoll += 1
+            break;
+        case 'saveFigure':
+            const result = await countResult(mug, chosenFigure);
+            game.players[userID].table = await saveFigure(currentPlayerTable, chosenFigure, result);
+            game.numberOfRoll = 0
+            game.currentPlayer = getNextPlayer(playerIDs, currentPlayer);
+            game.numberOfTurn += getTurn(playerIDs, currentPlayer, indexOfFirstPlayer)
+            game.isActive = isGameEnd(numberOfTurn)
+            break;
     }
 }
