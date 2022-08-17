a22b955660723e48def7b71cacb61c976d1dfcdc
Bartosz Lenrtowicz <bartek.lenart95@gmail.com>
2022-08-17 18:19:18 +0200

    Colorize user during create game

--- gui/src/components/create/Create.js
+++ gui/src/components/create/Create.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Container, ListGroup, Button } from 'react-bootstrap';
 
+import User from './user/user'
+
 export default props => {
 
   const [currentUser, setCurrentUser] = useState({})
   const [users, setUsers] = useState([])
-  const [selectedUsers, setSelectedUsers] = useState([])
 
   const getUsers = async () => {
     try {
@@ -29,15 +30,26 @@ export default props => {
 
   useEffect(() => { getUsers() })
 
-  const selectUser = user => {
-    const index = selectedUsers.indexOf(user.id)
-    index === -1 ? selectedUsers.push(user.id) : selectedUsers.splice(index, 1)
-    setSelectedUsers(selectedUsers)
+  const selectUser = selectedUser => {
+    const a = users.map(user => {
+      if(user.id === selectedUser.id) {
+        if(!user.selected) {
+          user.selected = true
+        } else {
+          user.selected = false
+        }
+        return user 
+      } else {
+        return user
+      }
+    })
+    setUsers(a)
   }
 
   const createGame = async () => {
     try {
       if(props.keycloak.authenticated) {
+        let selectedUsers = users.filter(user => user.selected === true).map(user => user.id)
         selectedUsers.push(currentUser.sub)
         const requestOptions = {
           method: 'POST',
@@ -56,14 +68,11 @@ export default props => {
     }
   }
 
-  const playerlist = users.map(user =>  <ListGroup.Item
-      key={user.id}
-      onClick={() => {selectUser(user)}}
-      className={selectedUsers.indexOf(user.id) === -1 ? "outline-success": "success"}
-      action>
-        {user.username}
-    </ListGroup.Item>) 
-
+  const userList = users.map(user => <User
+    key={user.id}
+    user_props={user}
+    selectUser={selectUser.bind(this)}
+  />)
 
   const createButton = <Button
     onClick={ createGame }
@@ -72,8 +81,9 @@ export default props => {
   </Button>
 
   return <><Container>
-    Selected users: {selectedUsers}
-    <ListGroup>{ playerlist } </ListGroup>
+    Select users: 
+    { userList }
+     
     </Container>
     {createButton}
   </>
---
+++ gui/src/components/create/user/user.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Button } from 'react-bootstrap';
+import './user.css'
+
+export default props => {
+    return <Button 
+    onClick={() => props.selectUser(props.user_props)} 
+    variant={ props.user_props.selected ? "outline-success" : "outline-secondary"}
+    id={props.user_props.id}>
+    {props.user_props.username}
+</Button>}
