c06fd52ad5021d28d01746a839c68e95f9b7a061
Bartosz Lenrtowicz <bartek.lenart95@gmail.com>
2022-08-25 15:09:07 +0200

    Add installation from github

---
+++ k8s/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -rf dice-game.yaml
+for dir in postgres ckey mongo api gui
+do
+    yamls=$(ls $dir)
+    for yaml in $yamls
+    do
+        cat $dir/$yaml >> dice-game.yaml
+        echo "\n---\n" >> dice-game.yaml
+    done
+done
+
+rm -rf dice-game-admin.yaml
+for dir in ckey-admin mongo-express
+do
+    yamls=$(ls $dir)
+    for yaml in $yamls
+    do
+        cat $dir/$yaml >> dice-game-admin.yaml
+        echo "\n---\n" >> dice-game-admin.yaml
+    done
+done
