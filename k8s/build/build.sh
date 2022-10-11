a36818883df24155eab3ab8a0ee931bfb40f773c
Bartosz Lenrtowicz <bartek.lenart95@gmail.com>
2022-10-11 19:54:30 +1100

    Move build files to separate directory

--- k8s/build/build.sh
+++ k8s/build/build.sh
@@ -3,10 +3,10 @@
 rm -rf dice-game.yaml
 for dir in postgres ckey mongo api gui
 do
-    yamls=$(ls $dir)
+    yamls=$(ls ../$dir)
     for yaml in $yamls
     do
-        cat $dir/$yaml >> dice-game.yaml
+        cat "../$dir/$yaml" >> dice-game.yaml
         echo "\n---\n" >> dice-game.yaml
     done
 done
@@ -14,10 +14,10 @@ done
 rm -rf dice-game-admin.yaml
 for dir in ckey-admin mongo-express
 do
-    yamls=$(ls $dir)
+    yamls=$(ls ../$dir)
     for yaml in $yamls
     do
-        cat $dir/$yaml >> dice-game-admin.yaml
+        cat ../$dir/$yaml >> dice-game-admin.yaml
         echo "\n---\n" >> dice-game-admin.yaml
     done
 done
